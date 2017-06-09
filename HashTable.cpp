c0bbb76f8990a84ae7c735f5dd5b3479a45e1f7c
Harish Palani <harishpalani3@gmail.com>
2017-06-09 02:26:08 +0000

    All fundamental hash table functions implemented

--- HashTable.cpp
+++ HashTable.cpp
@@ -22,7 +22,33 @@ Student* HashTable::find(int id) {
 }
 
 void HashTable::add(char* firstName, char* lastName, int id, float gpa) {
+    Node** current = &table[id % size];
+    int count = 0;
     
+    // Count elements in hash table
+    while (*current != 0) {
+        current = &(*current)->next;
+        count++;
+    }
+    
+    // If >= 3 elements in table...
+    if (count >= 3) {
+        size *= 2;
+        Node** old = list;
+        list = new Node*[size];
+        
+        for (int i = 0; i < size / 2; i++) {
+            add(current->data);
+            Node* temp = current;
+            current = current->next;
+            delete temp;
+        }
+        
+        delete[] old;
+        add(firstName, lastName, id, gpa);
+    } else {
+        *current = new Node(new Student(firstName, lastName, id, gpa));
+    }
 }
 
 bool HashTable::remove(int id) {
